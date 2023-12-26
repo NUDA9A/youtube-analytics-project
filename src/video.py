c987b015f77de16afbe0420b5ae5dbaa8342210d
Akimenko_Ivan <412871@niuitmo.ru>
2023-12-26 21:17:08 +0300

    HW 5

--- src/video.py
+++ src/video.py
@@ -11,10 +11,16 @@ class Video:
         self.video_id = video_id
         video = Video.__youtube.videos().list(part="snippet,statistics", id=video_id).execute()
         info = json.loads(json.dumps(video, indent=2, ensure_ascii=False))
-        self.title = info['items'][0]['snippet']['title']
-        self.url = "https://www.youtube.com/watch?v=" + info['items'][0]['id']
-        self.view_count = info['items'][0]['statistics']['viewCount']
-        self.like_count = info['items'][0]['statistics']['likeCount']
+        try:
+            self.title = info['items'][0]['snippet']['title']
+            self.url = "https://www.youtube.com/watch?v=" + info['items'][0]['id']
+            self.view_count = info['items'][0]['statistics']['viewCount']
+            self.like_count = info['items'][0]['statistics']['likeCount']
+        except IndexError:
+            self.title = None
+            self.url = None
+            self.view_count = None
+            self.like_count = None
 
     def __str__(self):
         return self.title
